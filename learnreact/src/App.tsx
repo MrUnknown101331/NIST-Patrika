a92e966f3700d2cae06364cde9a3b47edb54db96
MrUnknown101331 <faizanahmed0107@gmail.com>
2024-09-02 01:39:35 +0530

    Initial commit

---
+++ learnreact/src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./Header/Header.tsx";
+import NavPanel from "./Nav/NavPanel.tsx";
+
+function App() {
+    return (
+        <>
+            <Header/>
+            <NavPanel/>
+        </>
+    );
+}
+
+export default App;
